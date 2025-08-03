44e4eadc3b63a3c9cd1f8ea7d79443ac54bf15ff
Amin Ogarrio <amin.ogarrio@gmail.com>
2025-08-02 17:37:58 -0700

    send to master

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('El sordo es joto funcionando en puerto 3000\n');
+  res.end('funcionando en puerto 3000\n');
 });
 
 server.listen(3000, () => {
-  console.log('Servidor ejecutándose en http://localhost:3000');
+  console.log(' en http://localhost:3000');
 });
+
+
+
